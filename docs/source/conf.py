73c12085244da62baa410ef495a81d83d5408e6b
ckoerner-aur <67602165+ckoerner-aur@users.noreply.github.com>
2021-12-28 12:23:12 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ReAX Documentation'
+copyright = '2022, Aurora Multimedia Corp'
+author = 'CK'
 
 release = '0.1'
 version = '0.1.0'
